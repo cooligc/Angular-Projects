ef71c00555be80662d71f786798a34f98d13d74f
Sitakanta Chaudhury <Sitakanta Chaudhury>
2016-11-05 03:49:48 +0530

    Initial Commit for app1

---
+++ Angular/app2/js/controller.js
@@ -0,0 +1,6 @@
+var module = angular.module("app",["testMod"]);
+
+module.controller("ShowCtrl",function($scope){
+	
+});
+
---
+++ Angular/app2/js/controller2.js
@@ -0,0 +1,5 @@
+var a = angular.module("testMod",[]);
+
+a.controller("IterCtrl",function(){
+	this.mylist=[1,2,3,4];
+});
